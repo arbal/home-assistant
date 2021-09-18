ec1f7f6970a17f27c3a17bea0b5de856a2723d4d
Arbal <1980389+arbal@users.noreply.github.com>
2021-09-18 08:29:17 -0700

    hardcode VMID to 96

--- hassos_install.sh
+++ hassos_install.sh
@@ -76,7 +76,8 @@ fi
 info "Using '$STORAGE' for storage location."
 
 # Get the next guest VM/LXC ID
-VMID=$(pvesh get /cluster/nextid)
+#VMID=$(pvesh get /cluster/nextid)
+VMID="96"
 info "Container ID is $VMID."
 
 # Get latest Home Assistant disk image archive URL
